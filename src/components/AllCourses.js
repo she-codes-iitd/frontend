74c72668738030127e77ccca339f5e04b1c044c7
Sreemanti Dey <sreemantidey1234@gmail.com>
2021-12-25 14:42:24 +0530

    all-courses fetch done

--- src/components/AllCourses.js
+++ src/components/AllCourses.js
@@ -1,11 +1,17 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
 import '../css/All.css';
 import Header2 from './Header2';
 import id_page from '../imgs/Group19.png'
 
+import React, {useEffect ,useState} from 'react'
+import axios from 'axios';
 function AllCourses(props) {
-
+	const [courses, setCourses] = useState([]);
+	useEffect(() => {
+		fetch('http://localhost:3000/courses/')
+			.then(res => res.json())
+			.then(data => setCourses(data["courses"]))
+	}, []);
 	return (
 		<>
 			<div id="contain">
@@ -13,40 +19,18 @@ function AllCourses(props) {
 				<Header2 logo={props.logo} hamburger={props.hamburger} navMenu={props.navMenu} setHamburger={props.setHamburger} setNav={props.setNav} />
 			</div>
 			<div class="grid-container-all">
-				<div class="grid-item-all">1
-				<button className='btn btn-primary'>Enroll</button>
-				</div>
-				<div class="grid-item-all">2
-				<button className='btn btn-primary'>Enroll</button>
-				</div>
-				<div class="grid-item-all">3
-				
-				<button className='btn btn-primary'>Enroll</button>
-				</div>
-				<div class="grid-item-all">4
-				<button className='btn btn-primary'>Enroll</button>
-				</div>
-				<div class="grid-item-all">5
-				
-				<button className='btn btn-primary'>Enroll</button>
-				</div>
-				<div class="grid-item-all">6
-				<button className='btn btn-primary'>Enroll</button>
-				</div>
-				<div class="grid-item-all">7
-				<button className='btn btn-primary'>Enroll</button>
-				</div>
-				<div class="grid-item-all">8
-				<button className='btn btn-primary'>Enroll</button>
-				</div>
-				<div class="grid-item-all">9
-				
-				<button className='btn btn-primary'>Enroll</button>
-				</div>
+				{courses.map((course) => {
+					return (<div class="grid-item-all">{course["name"]}
+						<button className='btn btn-primary'>Enroll</button>
+					</div>
+					);
+				})}
 			</div>
 
 		</>
 	);
+
+
 }
 
 export default AllCourses;
